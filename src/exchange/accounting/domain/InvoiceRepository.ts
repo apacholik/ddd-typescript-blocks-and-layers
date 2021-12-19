bb60a4f6334cfce5bf98279cf6f17b338066fe44
apacholik <andrzej.pacholik@gmail.com>
2021-12-20 00:57:32 +0100

    Refactoring Invoice

--- src/exchange/accounting/domain/InvoiceRepository.ts
+++ src/exchange/accounting/domain/InvoiceRepository.ts
@@ -1,8 +1,6 @@
 import { Invoice } from "./Invoice";
-import { InvoiceNo } from "./InvoiceNo";
 
 export interface InvoiceRepository {
   save: (invoice: Invoice) => void;
-  //NOTE: Response should be Invoice.
-  get: (number: InvoiceNo) => boolean;
+  get: (number: string) => Invoice;
 }
--- src/exchange/accounting/infrastructure/DBInvoiceRepository.ts
+++ src/exchange/accounting/infrastructure/DBInvoiceRepository.ts
@@ -1,11 +1,25 @@
 import { Invoice } from "../domain/Invoice";
-import { InvoiceNo } from "../domain/InvoiceNo";
 import { InvoiceRepository } from "../domain/InvoiceRepository";
+import { InvoiceFactory } from "../domain/InvoiceFactory";
+import { PositionAttributes } from "../domain/PositionAttributes";
+import { InvoicePosition } from '../application/InvoicePosition';
+import { PLNPositionLimitPolicy } from '../domain/policy/PLNPositionLimitPolicy';
 
 export class DBInvoiceRepository implements InvoiceRepository {
-  //NOTE: This should return Invoice
-  get(number: InvoiceNo): boolean {
-    return false;
+  private invoiceFactory: InvoiceFactory = new InvoiceFactory();
+
+  get(number: string): Invoice {
+    // NOTE: This data should get from DB
+    const dbData = {
+        value: 245,
+        currency: "PLN",
+      }
+
+
+    const positions: PositionAttributes = new InvoicePosition(dbData.value, dbData.currency);
+
+    const invoice = this.invoiceFactory.createInvoice([positions], number, new PLNPositionLimitPolicy())
+    return invoice;
   }
 
   save(invoice: Invoice): void {
